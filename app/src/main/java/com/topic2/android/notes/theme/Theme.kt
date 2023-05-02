3fa1990e8c1518923c18e23446afd07b66a647fd
JayS <krazynik016@gmail.com>
2023-05-02 23:06:57 +0300

    зем

--- app/src/main/java/com/topic2/android/notes/theme/Theme.kt
+++ app/src/main/java/com/topic2/android/notes/theme/Theme.kt
@@ -2,11 +2,13 @@ package com.topic2.android.notes.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 private val LightThemeColors = lightColors(
   primary = rwGreen,
@@ -14,15 +16,14 @@ private val LightThemeColors = lightColors(
   secondary = rwGreen
 )
 
-private val DarkThemeColors = lightColors(
-  primary = rwGreen,
-  primaryVariant = rwGreenDark,
-  secondary = rwGreen
+private val DarkThemeColors = darkColors(
+  primary = Color( 0xFF00A55),
+  primaryVariant = Color(0xFF00F884),
+  secondary = rwGreen,
+  onPrimary = Color.White,
 )
 
-/**
- * Отвечает за переключение цветовой палитры для темной и светлой темы.
- */
+
 @Composable
 fun NotesTheme(content: @Composable () -> Unit) {
   val isDarkThemeEnabled = isSystemInDarkTheme() || NotesThemeSettings.isDarkThemeEnabled
@@ -31,9 +32,7 @@ fun NotesTheme(content: @Composable () -> Unit) {
   MaterialTheme(colors = colors, content = content)
 }
 
-/**
- * Позволяет переключаться между светлой и темной темой в настройках приложения.
- */
+
 object NotesThemeSettings {
   var isDarkThemeEnabled by mutableStateOf(false)
 }
